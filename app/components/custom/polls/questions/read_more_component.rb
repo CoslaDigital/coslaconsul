5cdfaf16e13b6eec92277473c380eee661f4222a
CoslaDeploy <consul@cosla.gov.uk>
2023-08-10 09:24:38 +0000

    updating component logic for custom component view

---
+++ app/components/custom/polls/questions/read_more_component.rb
@@ -0,0 +1,17 @@
+class Polls::Questions::ReadMoreComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "admin", "read_more_component").to_s
+
+class Polls::Questions::ReadMoreComponent
+  with_collection_parameter :question
+  attr_reader :question
+  delegate :wysiwyg, to: :helpers
+
+  def initialize(question:)
+    @question = question
+  end
+
+  def render?
+    question.answers_with_read_more?
+  end
+end
