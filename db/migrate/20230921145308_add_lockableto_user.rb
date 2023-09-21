b9adc249bf691951656fbd93d5d2530ed7e8f254
CoslaJohn <johnmunro@cosla.gov.uk>
2023-09-21 17:15:16 +0100

    enabled lockable in devise and added to user model

---
+++ db/migrate/20230921145308_add_lockableto_user.rb
@@ -0,0 +1,8 @@
+class AddLockabletoUser < ActiveRecord::Migration[6.0]
+  def change
+  ## Lockable
+    add_column :users, :failed_attempts, :integer, default: 0, null: false
+    add_column :users, :unlock_token, :string
+    add_column :users, :locked_at, :datetime
+  end
+end
